39115068cf694433559811c457d937d7ec0860ec
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-01-07 22:39:09 +0900

    add doc lab1.cs

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -10,11 +10,5 @@ namespace Lab1
         }
     }
 
-    public static class Lab1
-    {
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
-    }
+    
 }
