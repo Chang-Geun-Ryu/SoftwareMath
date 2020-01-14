d43daf7098a6d3dcbf1bf8f638a11e3985bccc78
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-01-14 23:23:20 +0900

    test a1

---
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class BigNumberCalculator
+    {
+        public BigNumberCalculator(int bitCount, EMode mode)
+        {
+        }
+
+        public static string GetOnesComplementOrNull(string num)
+        {
+            if (num.Contains("0b"))
+            {
+                CharEnumerator ch = num.Substring(2).GetEnumerator();
+                string strBinary = "0b";
+
+                while(ch.MoveNext())
+                {
+                    if(ch.Current.Equals('0'))
+                    {
+                        strBinary += "1";
+                    } else if (ch.Current.Equals('1'))
+                    {
+                        strBinary += "0";
+                    } else
+                    {
+                        return null;
+                    }
+                }
+
+                return strBinary;
+            }
+            return null;
+        }
+
+        public static string GetTwosComplementOrNull(string num)
+        {
+            string strOnce = GetOnesComplementOrNull(num);
+
+            if (strOnce.Length > 0)
+            {
+                char[] chArray = strOnce.ToCharArray();
+                bool addOne = true;
+                int index = chArray.Length - 1;
+
+                while (index > 0)
+                {
+                    if (chArray[index].Equals('0'))
+                    {
+                        if (addOne)
+                        {
+                            chArray[index] = '1';
+                            addOne = false;
+                        }
+                    }
+                    else if (chArray[index].Equals('1'))
+                    {
+                        if (addOne)
+                        {
+                            chArray[index] = '0';
+                            addOne = true;
+                        }
+                    }
+                    else
+                    {
+                        return new string(chArray); //chArray.ToString();
+                    }
+
+                    --index;
+                }
+            }
+            return null;
+        }
+
+        public static string ToBinaryOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToHexOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToDecimalOrNull(string num)
+        {
+            return null;
+        }
+
+        public string AddOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+
+        public string SubtractOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+    }
+}
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -6,7 +6,13 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("random string")); // null
+            //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("159")); // null
+            //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("0xFF34F")); // null
+            //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("0b0110101011101011100000")); // 0b1001010100010100011111
+
+            Console.WriteLine("0b0000111010110 ->" + BigNumberCalculator.GetTwosComplementOrNull("0b0000111010110")); // 0b1111000101010
+            Console.WriteLine("0b1000 ->" + BigNumberCalculator.GetTwosComplementOrNull("0b1000")); // 0b1000
         }
     }
 }
