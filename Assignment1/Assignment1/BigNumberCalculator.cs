aba37fc93bc3a28f72829c1e50a80938682b28d9
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-01-18 01:46:58 +0900

    adding

--- Assignment1/Assignment1/BigNumberCalculator.cs
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -17,13 +17,15 @@ namespace Assignment1
 
                 while(ch.MoveNext())
                 {
-                    if(ch.Current.Equals('0'))
+                    if (ch.Current.Equals('0'))
                     {
                         strBinary += "1";
-                    } else if (ch.Current.Equals('1'))
+                    }
+                    else if (ch.Current.Equals('1'))
                     {
                         strBinary += "0";
-                    } else
+                    }
+                    else
                     {
                         return null;
                     }
@@ -41,30 +43,30 @@ namespace Assignment1
             if (strOnce.Length > 0)
             {
                 char[] chArray = strOnce.ToCharArray();
-                bool addOne = true;
+                bool bAddOne = true;
                 int index = chArray.Length - 1;
 
                 while (index > 0)
                 {
                     if (chArray[index].Equals('0'))
                     {
-                        if (addOne)
+                        if (bAddOne)
                         {
                             chArray[index] = '1';
-                            addOne = false;
+                            bAddOne = false;
                         }
                     }
                     else if (chArray[index].Equals('1'))
                     {
-                        if (addOne)
+                        if (bAddOne)
                         {
                             chArray[index] = '0';
-                            addOne = true;
+                            bAddOne = true;
                         }
                     }
                     else
                     {
-                        return new string(chArray); //chArray.ToString();
+                        return new string(chArray);
                     }
 
                     --index;
@@ -75,6 +77,80 @@ namespace Assignment1
 
         public static string ToBinaryOrNull(string num)
         {
+            if (num.Contains("0b"))
+            {
+                return num;
+            }
+            else if (num.Contains("0x"))
+            {
+                char[] chArray = num.ToCharArray();
+                int index = chArray.Length - 1;
+                string sBinary = "";
+
+                while (index > 0)
+                {
+                    sBinary = HexToBinary(chArray[index]) + sBinary;
+
+                    index--;
+                }
+
+                return "0b" + sBinary;
+            }
+            else
+            {
+                int number = Math.Abs(Int32.Parse(num));
+                int pow = 0;
+                bool first = true;
+                string sbinary = "";
+
+                while (true)
+                {
+                    if (first)
+                    {
+                        int value = (int)Math.Pow(2, pow);
+                        if ((number - value) > 0)
+                        {
+                            pow++;
+                        }
+                        else
+                        {
+                            first = false;
+                        }
+                    }
+                    else
+                    {
+                        int value = (int)Math.Pow(2, pow);
+                        if ((number - value) >= 0)
+                        {
+                            number -= value;
+                            
+                            sbinary += "1";
+                        }
+                        else
+                        {
+                            sbinary += "0";
+                        }
+
+                        pow--;
+
+                        if (number <= 0 || pow < 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+                if (num.Contains('-'))
+                {
+                    sbinary = GetTwosComplementOrNull("0b" + sbinary);
+                }
+                else
+                {
+                    sbinary = "0b" + sbinary;
+                }
+
+                return sbinary;
+            }
             return null;
         }
 
@@ -97,6 +173,75 @@ namespace Assignment1
         public string SubtractOrNull(string num1, string num2, out bool bOverflow)
         {
             bOverflow = false;
+            return null;
+        }
+
+        public static string HexToBinary(char hex)
+        {
+            if (hex.Equals('0'))
+            {
+                return "0000";
+            }
+            else if (hex.Equals('1'))
+            {
+                return "0001";
+            }
+            else if (hex.Equals('2'))
+            {
+                return "0010";
+            }
+            else if (hex.Equals('3'))
+            {
+                return "0011";
+            }
+            else if (hex.Equals('4'))
+            {
+                return "0100";
+            }
+            else if (hex.Equals('5'))
+            {
+                return "0101";
+            }
+            else if (hex.Equals('6'))
+            {
+                return "0110";
+            }
+            else if (hex.Equals('7'))
+            {
+                return "0111";
+            }
+            else if (hex.Equals('8'))
+            {
+                return "1000";
+            }
+            else if (hex.Equals('9'))
+            {
+                return "1001";
+            }
+            else if (hex.Equals('A'))
+            {
+                return "1010";
+            }
+            else if (hex.Equals('B'))
+            {
+                return "1011";
+            }
+            else if (hex.Equals('C'))
+            {
+                return "1100";
+            }
+            else if (hex.Equals('D'))
+            {
+                return "1101";
+            }
+            else if (hex.Equals('E'))
+            {
+                return "1110";
+            }
+            else if (hex.Equals('F'))
+            {
+                return "1111";
+            }
             return null;
         }
     }
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -6,6 +6,17 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+
+            //if ('1' > '2')
+            //{
+            //    Console.WriteLine("ddd");
+            //}
+            //else
+            //{
+            //    int a = '9';
+            //    Console.WriteLine("ffff");
+            //}
+
             //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("random string")); // null
             //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("159")); // null
             //Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull("0xFF34F")); // null
@@ -13,6 +24,11 @@ namespace Assignment1
 
             Console.WriteLine("0b0000111010110 ->" + BigNumberCalculator.GetTwosComplementOrNull("0b0000111010110")); // 0b1111000101010
             Console.WriteLine("0b1000 ->" + BigNumberCalculator.GetTwosComplementOrNull("0b1000")); // 0b1000
+
+            Console.WriteLine("0b00001101011 ->" + BigNumberCalculator.ToBinaryOrNull("0b00001101011")); // 0b00001101011
+            Console.WriteLine("0x00F24 ->" + BigNumberCalculator.ToBinaryOrNull("0x00F24")); // 0b00000000111100100100
+            Console.WriteLine("123 ->" + BigNumberCalculator.ToBinaryOrNull("123")); // 0b01111011
+            Console.WriteLine("-123 ->" + BigNumberCalculator.ToBinaryOrNull("-123")); // 0b10000101
         }
     }
 }
