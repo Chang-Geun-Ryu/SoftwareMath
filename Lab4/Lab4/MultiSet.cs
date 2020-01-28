1bee65a91c8faf018800537e3ce1b310beec7586
ryu <goodrcg@gmail.com>
2020-01-28 18:33:33 +0900

    rkwmdk

--- Lab4/Lab4/MultiSet.cs
+++ Lab4/Lab4/MultiSet.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab4
 {
@@ -32,22 +34,102 @@ namespace Lab4
 
         public MultiSet Union(MultiSet other)
         {
-            return null;
+            var otherList = other.ToList();
+            MultiSet set = new MultiSet();
+            HashSet<string> tempSet = new HashSet<string> { };
+
+            foreach (string element in mList)
+            {
+                tempSet.Add(element);
+            }
+            foreach (string element in otherList)
+            {
+                tempSet.Add(element);
+            }
+
+            foreach (string element in tempSet)
+            {
+                set.Add(element);
+
+                uint nElementCount = this.GetMultiplicity(element);
+                uint nElementOtherCount = other.GetMultiplicity(element);
+                uint nMultiElement = nElementCount >= nElementOtherCount ? nElementCount : nElementOtherCount;
+
+                for (uint i = set.GetMultiplicity(element); i < nMultiElement; i++)
+                {
+                    set.Add(element);
+                }
+            }
+            
+            return set;
         }
 
         public MultiSet Intersect(MultiSet other)
         {
-            return null;
+            var otherList = other.ToList();
+            var thisList = this.mList;
+            MultiSet set = new MultiSet();
+            List<string> tempList = new List<string> { };
+
+            foreach (string otherElement in otherList)
+            {
+                foreach (string element in thisList)
+                {
+                    if (otherElement == element)
+                    {
+                        set.Add(element);
+                        thisList.Remove(element);
+                        break;
+                    }
+                }
+            }
+
+            return set;
         }
 
         public MultiSet Subtract(MultiSet other)
         {
-            return null;
+            var otherList = other.ToList();
+            var thisList = this.mList;
+            MultiSet set = new MultiSet();
+            List<string> tempList = new List<string> { };
+
+            var diffList = thisList.FindAll(element => otherList.FindIndex(x => x == element) == -1);
+
+            foreach (string element in diffList)
+            {
+                set.Add(element);
+            }
+
+            return set;
         }
 
         public List<MultiSet> FindPowerSet()
         {
-            return null;
+            List<MultiSet> powerSet = new List<MultiSet> { };
+
+            for (int i = 0; i < 1 << mList.Count; i++)
+            {
+                bool[] caseArray = getCaseBoolArray(i);
+                int idx = 0;
+                var subList = mList.FindAll(x => caseArray[idx++]);
+                MultiSet powElement = new MultiSet();
+
+                subList.ForEach(element => powElement.Add(element));
+
+                var overlapList = powerSet.Where(element => element.ToList().SequenceEqual(subList));
+
+                if (overlapList.Count() == 0)
+                {
+                    powerSet.Add(powElement);
+                }
+                
+            }
+
+            //Func<MultiSet, List<MultiSet>, bool> overlap = (finder, power) => 
+            //powerSet.Where(finder => { (finder.ToList(), powerSet) => true};) ;
+
+            return powerSet;
         }
 
         public bool IsSubsetOf(MultiSet other)
@@ -58,6 +140,56 @@ namespace Lab4
         public bool IsSupersetOf(MultiSet other)
         {
             return false;
+        }
+
+        private bool[] shiftBoolArray(bool[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] && arr[i+1] == false)
+                {
+                    arr[i] = false;
+                    arr[i + 1] = true;
+                    return arr;
+                }
+            }
+
+            arr[0] = true;
+            return arr;
+        }
+
+        private bool[] getCaseBoolArray(int nNumber)
+        {
+            bool[] bPowerFlagArr = Enumerable.Repeat(false, mList.Count).ToArray();
+
+            for (int i = 0; i < mList.Count; i++)
+            {
+                int nCase = 1 << i;
+                if ((nNumber & nCase) == nCase)
+                {
+                    bPowerFlagArr[i] = true;
+                }
+            }
+
+            return bPowerFlagArr;
+        }
+
+        private bool checkFalse(bool[] arr)
+        {
+            foreach (bool element in arr)
+            {
+                if (element == false)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool findEqual(MultiSet other)
+        {
+            return this.Subtract(other).ToList().Count > 0 ? true : false;
         }
     }
 }
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Lab4
 {
@@ -9,79 +11,109 @@ namespace Lab4
         {
             MultiSet set = new MultiSet();
 
-            set.Add("cattle");
-            set.Add("bee");
-            set.Add("cattle");
-            set.Add("bee");
-            set.Add("happy");
-            set.Add("zachariah");
+            set.Add("a");
+            set.Add("b");
+            set.Add("a");
 
-            Debug.Assert(set.Remove("zachariah"));
-            Debug.Assert(!set.Remove("fun"));
+            List<MultiSet> powersetList = set.FindPowerSet();
+            // powersetList:
+            // { }
+            // { a }
+            // { a, a }
+            // { a, a, b }
+            // { a, b }
+            // { b }
 
-            Debug.Assert(set.GetMultiplicity("cattle") == 2);
 
-            List<string> expectedList = new List<string> { "bee", "bee", "cattle", "cattle", "happy" };
-            List<string> list = set.ToList();
+            //MultiSet set1 = new MultiSet();
+            //MultiSet set2 = new MultiSet();
 
-            Debug.Assert(list.Count == 5);
+            //set1.Add("d");
+            //set1.Add("k");
+            //set1.Add("f");
+            //set1.Add("e"); // set1: { d, e, f, k }
 
-            for (int i = 0; i < expectedList.Count; i++)
-            {
-                Debug.Assert(expectedList[i] == list[i]);
-            }
+            //set2.Add("g");
+            //set2.Add("e");
+            //set2.Add("f"); // set2: { e, f, g }
 
-            MultiSet set2 = new MultiSet();
+            //MultiSet difference = set1.Subtract(set2); // difference: { d, k }
 
-            set2.Add("cattle");
-            set2.Add("cattle");
-            set2.Add("bee");
+            //MultiSet set = new MultiSet();
 
-            list = set.Union(set2).ToList();
-            Debug.Assert(list.Count == 5);
+            //set.Add("cattle");
+            //set.Add("bee");
+            //set.Add("cattle");
+            //set.Add("bee");
+            //set.Add("happy");
+            //set.Add("zachariah");
 
-            for (int i = 0; i < expectedList.Count; i++)
-            {
-                Debug.Assert(expectedList[i] == list[i]);
-            }
+            //Debug.Assert(set.Remove("zachariah"));
+            //Debug.Assert(!set.Remove("fun"));
 
-            expectedList = new List<string> { "bee", "cattle", "cattle" };
-            list = set.Intersect(set2).ToList();
-            Debug.Assert(list.Count == 3);
+            //Debug.Assert(set.GetMultiplicity("cattle") == 2);
 
-            for (int i = 0; i < expectedList.Count; i++)
-            {
-                Debug.Assert(expectedList[i] == list[i]);
-            }
+            //List<string> expectedList = new List<string> { "bee", "bee", "cattle", "cattle", "happy" };
+            //List<string> list = set.ToList();
 
-            expectedList = new List<string> { "bee", "happy" };
-            list = set.Subtract(set2).ToList();
-            Debug.Assert(list.Count == 2);
+            //Debug.Assert(list.Count == 5);
 
-            for (int i = 0; i < expectedList.Count; i++)
-            {
-                Debug.Assert(expectedList[i] == list[i]);
-            }
+            //for (int i = 0; i < expectedList.Count; i++)
+            //{
+            //    Debug.Assert(expectedList[i] == list[i]);
+            //}
 
-            List<MultiSet> expectedPowerset = getExpectedPowerset();
-            List<MultiSet> set2PowerSet = set2.FindPowerSet();
-            Debug.Assert(set2PowerSet.Count == expectedPowerset.Count);
+            //MultiSet set2 = new MultiSet();
 
-            for (int i = 0; i < expectedPowerset.Count; i++)
-            {
-                expectedList = expectedPowerset[i].ToList();
-                list = set2PowerSet[i].ToList();
+            //set2.Add("cattle");
+            //set2.Add("cattle");
+            //set2.Add("bee");
 
-                Debug.Assert(expectedList.Count == list.Count);
+            //list = set.Union(set2).ToList();
+            //Debug.Assert(list.Count == 5);
 
-                for (int j = 0; j < expectedList.Count; j++)
-                {
-                    Debug.Assert(expectedList[j] == list[j]);
-                }
-            }
+            //for (int i = 0; i < expectedList.Count; i++)
+            //{
+            //    Debug.Assert(expectedList[i] == list[i]);
+            //}
 
-            Debug.Assert(!set.IsSubsetOf(set2));
-            Debug.Assert(set.IsSupersetOf(set2));
+            //expectedList = new List<string> { "bee", "cattle", "cattle" };
+            //list = set.Intersect(set2).ToList();
+            //Debug.Assert(list.Count == 3);
+
+            //for (int i = 0; i < expectedList.Count; i++)
+            //{
+            //    Debug.Assert(expectedList[i] == list[i]);
+            //}
+
+            //expectedList = new List<string> { "bee", "happy" };
+            //list = set.Subtract(set2).ToList();
+            //Debug.Assert(list.Count == 2);
+
+            //for (int i = 0; i < expectedList.Count; i++)
+            //{
+            //    Debug.Assert(expectedList[i] == list[i]);
+            //}
+
+            //List<MultiSet> expectedPowerset = getExpectedPowerset();
+            //List<MultiSet> set2PowerSet = set2.FindPowerSet();
+            //Debug.Assert(set2PowerSet.Count == expectedPowerset.Count);
+
+            //for (int i = 0; i < expectedPowerset.Count; i++)
+            //{
+            //    expectedList = expectedPowerset[i].ToList();
+            //    list = set2PowerSet[i].ToList();
+
+            //    Debug.Assert(expectedList.Count == list.Count);
+
+            //    for (int j = 0; j < expectedList.Count; j++)
+            //    {
+            //        Debug.Assert(expectedList[j] == list[j]);
+            //    }
+            //}
+
+            //Debug.Assert(!set.IsSubsetOf(set2));
+            //Debug.Assert(set.IsSupersetOf(set2));
         }
 
         private static List<MultiSet> getExpectedPowerset()
