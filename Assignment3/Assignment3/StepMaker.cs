a294f9df9e742eda9953d0deb87ed801eb20323b
ryu <goodrcg@gmail.com>
2020-02-20 15:46:40 +0900

    Fix Assignment 3 code style

--- Assignment3/Assignment3/StepMaker.cs
+++ Assignment3/Assignment3/StepMaker.cs
@@ -15,7 +15,9 @@ namespace Assignment3
 
                 if (nAbs > 10)
                 {
-                    List<int> tempList = new List<int> {steps[i], steps[i + 1]};
+                    List<int> tempList = new List<int>();
+                    tempList.Add(steps[i]);
+                    tempList.Add(steps[i + 1]);
 
                     var temp = (makeStepTail(tempList, noise, 0));
                     stepList.AddRange(temp);
@@ -27,7 +29,6 @@ namespace Assignment3
             }
 
             stepList.Add(steps[steps.Length - 1]);
-            // return new List<int> (makeStepTail(steps, noise, 0));
             return stepList;
         }
 
@@ -38,24 +39,26 @@ namespace Assignment3
             
             if (checkSteps(steps.ToArray()))
             {
-                steps.RemoveAt(steps.Count-1);
+                steps.RemoveAt(steps.Count - 1);
                 return steps;
             }
 
             acc += 1;
-            List<int> result = new List<int> ();
+            List<int> result = new List<int>();
             for (int i = 0; i < steps.Count - 1; i++)
             {
                 int nAbs = Math.Abs(steps[i + 1] - steps[i]);
                 if (nAbs > 10)
                 {
-                    var temp = makeStepTail(new List<int> {steps[i], steps[i + 1]}, noise, acc);
-                    // temp.RemoveAt(temp.Count-1);
+                    List<int> tempList = new List<int>();
+                    tempList.Add(steps[i]);
+                    tempList.Add(steps[i + 1]);
+
+                    var temp = makeStepTail(tempList, noise, acc);
                     result.AddRange(temp);
                 }
                 else 
                 {
-                    
                     result.Add(steps[i]);
                 }
             }
@@ -67,14 +70,10 @@ namespace Assignment3
         {
             List<int> linearValues = new List<int> { };
 
-            for (int i = 1; i < 5; i++ )
+            for (int i = 1; i < 5; i++)
             {
-                // decimal start = nStart;
-                // decimal end = nEnd;
-                // decimal noiseResult = noise.GetNext(acc);
-                // decimal value = i;
-                // linearValues.Add((int)(noiseResult + start + (end - start) * (1.0m * value) / 5.0m));
-                int temp = noise.GetNext(acc) +  (int)((decimal)nStart + (decimal)((nEnd - nStart) * i) / 5.0m);
+                int temp = (int)((decimal)nStart + (decimal)((nEnd - nStart) * i) / 5.0m);
+                temp += noise.GetNext(acc);
                 linearValues.Add(temp);
             }
 
