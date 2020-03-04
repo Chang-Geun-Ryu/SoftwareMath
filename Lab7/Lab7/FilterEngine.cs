6ccc300cb28aa679ab763b8dacc219329a661116
ryu <goodrcg@gmail.com>
2020-03-04 15:47:04 +0900

    lab7 update

--- Lab7/Lab7/FilterEngine.cs
+++ Lab7/Lab7/FilterEngine.cs
@@ -25,8 +25,8 @@ namespace Lab7
         {
             return list.ConvertAll(p => 
                 {
-                    int nMatch = 0;
-                    int nPriority = featureList.Count;
+                    int nMatch = -1;
+                    int nPriority = featureList.Count - 1;
                     int nSortKey = 0;
 
                     featureList.ForEach(
@@ -41,12 +41,39 @@ namespace Lab7
                             Console.WriteLine($"bit: {Convert.ToString(nSortKey, 2).PadLeft(32, '0')}");
                         });
                     
-                    nSortKey |= nMatch == 0 ? 0 : (1 << featureList.Count + nMatch);
+                    nSortKey |= nMatch == -1 ? 0 : (1 << featureList.Count + nMatch);
                     Console.WriteLine($"result5: {Convert.ToString(nSortKey, 2).PadLeft(32, '0')}");
                     Console.WriteLine($"p: {p.Name}, p Count: {featureList.Count}, nMatch: {nMatch}, sortKey: {nSortKey}");
                     return nSortKey;
                 });
         }
+
+        // public static List<int> GetSortKeys(List<Frame> list, List<EFeatureFlags> featureList)
+        // {
+        //     return list.ConvertAll(p => 
+        //         {
+        //             int nMatch = 0;
+        //             int nPriority = featureList.Count;
+        //             int nSortKey = 0;
+
+        //             featureList.ForEach(
+        //                 feature => 
+        //                 {
+        //                     if ((feature & p.Features) != EFeatureFlags.Default)
+        //                     {
+        //                         nMatch++;
+        //                         nSortKey += nPriority;
+        //                     }
+        //                     nPriority--;
+        //                     Console.WriteLine($"bit: {Convert.ToString(nSortKey, 2).PadLeft(32, '0')}");
+        //                 });
+                    
+        //             nSortKey |= nMatch == 0 ? 0 : (1 << featureList.Count + nMatch);
+        //             Console.WriteLine($"result5: {Convert.ToString(nSortKey, 2).PadLeft(32, '0')}");
+        //             Console.WriteLine($"p: {p.Name}, p Count: {featureList.Count}, nMatch: {nMatch}, sortKey: {nSortKey}");
+        //             return nSortKey;
+        //         });
+        // }
 
     }
 }
