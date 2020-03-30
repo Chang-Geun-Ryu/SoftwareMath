e0541f8b840844b84468057bae45502cceea8dc0
ryu <goodrcg@gmail.com>
2020-03-30 18:03:51 +0900

    Setting Project Assignment4

--- Assignment4/Assignment4/SignalProcessor.cs
+++ Assignment4/Assignment4/SignalProcessor.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿
+using System.Drawing;
 
 namespace Assignment4
 {
-    public class SignalProcessor
+    public static class SignalProcessor
     {
-        public static int[] GetGaussianFilter1D(double sigma)
+        public static double[] GetGaussianFilter1D(double sigma)
         {
+            return null;
+        }
 
+        public static double[] Convolve1D(double[] signal, double[] filter)
+        {
+            return null;
+        }
+
+        public static double[,] GetGaussianFilter2D(double sigma)
+        {
+            return null;
+        }
+
+        public static Bitmap ConvolveImage(Bitmap bitmap, double[,] filter)
+        {
             return null;
         }
     }
-
 }
