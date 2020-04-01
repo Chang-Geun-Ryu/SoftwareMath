328c3dcd62c0e8dbcc0db1173ca6a0cd939f7d0a
ryu <goodrcg@gmail.com>
2020-04-01 16:33:58 +0900

    Update Ass 4

--- Assignment4/Assignment4/SignalProcessor.cs
+++ Assignment4/Assignment4/SignalProcessor.cs
@@ -28,6 +28,8 @@ namespace Assignment4
             double[] resultArr = new double[signal.Length];
             var center = filter.Length / 2;
 
+            Array.Reverse(filter);
+
             for (int i = 0; i < resultArr.Length; i++)
             {
                 double sum = 0d;
@@ -65,9 +67,74 @@ namespace Assignment4
 
         public static Bitmap ConvolveImage(Bitmap bitmap, double[,] filter)
         {
-            Color pixelColor = bitmap.GetPixel(0,0);
+            double[,] flipArr = flipMatrix(filter);
 
-            return null;
+            for (int w = 0; w < bitmap.Width; w++)
+            {
+                for (int h = 0; h < bitmap.Height; h++)
+                {
+                    Color color = getColorMatrix(bitmap, flipArr, w, h);
+                    
+                    bitmap.SetPixel(w, h, color);
+                }
+            }
+
+            return bitmap;
+        }
+
+        private static Color getColorMatrix(Bitmap bitmap, double[,] filter, int posX, int posY)
+        {
+            Color[,] colorArr = new Color[filter.GetLength(1), filter.GetLength(0)];
+
+            int centerX = filter.GetLength(0) / 2;
+            int centerY = filter.GetLength(1) / 2;
+            
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            for (int w = 0; w < filter.GetLength(0); w++)
+            {
+                for (int h = 0; h < filter.GetLength(1); h++)
+                {
+                    int indexX = posX - (centerX - w);
+                    int indexY = posY - (centerY - h);
+
+                    if (indexX < 0 || indexY < 0 || indexX >= bitmap.Width || indexY >= bitmap.Height)
+                    {
+                        colorArr[w, h] = Color.Black;
+                    }
+                    else 
+                    {
+                        colorArr[w, h] = bitmap.GetPixel(indexX, indexY);
+                    }
+
+                    // Console.WriteLine("({0},{1}: {2})", indexX, indexY, colorArr[w, h]);
+
+                    r += (int)((double)colorArr[w, h].R * filter[w, h]);
+                    g += (int)((double)colorArr[w, h].G * filter[w, h]);
+                    b += (int)((double)colorArr[w, h].B * filter[w, h]);
+                }
+            }
+
+            Color resultColor = Color.FromArgb(r, g, b);
+            return resultColor;
+        }
+
+
+        private static double[,] flipMatrix(double[,] filter)
+        {
+            double[,] flipArr = new double[filter.GetLength(1), filter.GetLength(0)];
+
+            for (int w = 0; w < filter.GetLength(1); w++)
+            {
+                for(int h = 0; h < filter.GetLength(0); h++)
+                {
+                    flipArr[w, h] = filter[filter.GetLength(1) - 1 - w,filter.GetLength(0) - 1 - h];
+                }
+            }
+
+            return flipArr;
         }
     }
 }
